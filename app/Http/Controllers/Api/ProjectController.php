e64e18d3a442c40b936269ce282b90bfe1bd1462
matteo-cota <matteocota@outlook.it>
2024-11-14 17:31:25 +0100

    milestone 1

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\JsonResponse;
+
+class ProjectController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $projects = Project::all();
+        return response()->json($projects);
+    }
+}
